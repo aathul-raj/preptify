b4dc4025d3a4298b784d6e5dae64609c0389951f
aathul.raj <athulrsuresh@gmail.com>
2023-07-06 16:25:13 -0500

    [FRONT-END] made dashboard more responsive

--- src/components/dashboard/Analytics.jsx
+++ src/components/dashboard/Analytics.jsx
@@ -6,11 +6,11 @@ import Graph from "./Graph"
 export default function Analytics(){
     return <>
         <CategoryScores/>
-        <Tile name="average-time" heading="Average Time to Answer (seconds)" subheading="12% Increase from Last Week" score={39} status="increase"/>
+        <Tile name="average-time" heading="Seconds to Answer" subheading="12% Increase from Last Week" score={39} status="increase"/>
         <Tile name="overall-score" heading="Overall Score" subheading="3% Decrease from Last Week" score={9.3} status="decrease"/>
         <Graph/>
         <RecentFeedback/>
         <Tile name="minutes-per-day" heading="Minutes Interviewing Per Day" subheading="12% Increase from Last Week" score={52} status="increase"/>
-        <Tile name="interviews-completed" heading="Interviews Completed" subheading="Keep it going!" score={150} status="increase"/>
+        <Tile name="interviews-completed" heading="Interviews Completed" subheading="Keep it going!" score={50} status="increase"/>
     </>
 }
