18b385fa2bc94c14dbf7788c99dc06463f49f69f
aathul.raj <athulrsuresh@gmail.com>
2023-08-01 18:20:56 -0500

    [BACK-END] basic stripe integration

---
+++ src/components/StandardCheckout.jsx
@@ -0,0 +1,23 @@
+import getStripe from "../back-end/getStripe";
+
+export default function StandardCheckout() {
+
+    async function handleCheckout(){
+        const stripe = await getStripe()
+        const { error } = await stripe.redirectToCheckout({
+            lineItems: [
+                {
+                    price: import.meta.env.VITE_STRIPE_PRICE_ID,
+                    quantity: 1,
+                }
+            ],
+            mode: 'subscription',
+            successUrl: 'http://localhost:5173/payment-confirmed',
+            cancelUrl: 'http://localhost:5173/about',
+            // ADD FUNCTIONALITY TO MAKE SURE USER IS SIGNED IN SO YOU CAN PUT customerEmail: 'customer@email.com',
+        })
+        console.warn(error.message);
+    }
+  
+    return <button onClick={handleCheckout}>subscribe</button>;
+  }
---
+++ src/views/PaymentConfirmed.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import MainHeader from "../components/global/MainHeader"
+import Footer from "../components/global/Footer"
+import Check from "../img/icons/green-check.png"
+import styles from "../styles/PaymentConfirmed.module.css"
+import { useNavigate } from "react-router-dom";
+
+export default function PaymentConfirmed() {
+    
+    let navigate = useNavigate()
+    return (
+        <>
+        <main className={styles["about-main"]}>
+            <MainHeader/>
+            <div className={styles["payment-content"]}>
+                <img src={Check} alt=""/>
+                <h1>Thanks for subscribing!</h1>
+                <h2>A payment to www.preptify.com will appear on your statement.</h2>
+                <h2><span onClick={() => navigate("/login")}>Login</span> to your upgraded account.</h2>
+            </div>
+            <Footer/>
+        </main>
+        </>
+    )
+    }
