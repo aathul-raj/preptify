0eb3c690ff9e411f655dea24fb0bbb4896bb01f7
SamYu0310 <shy1@rice.edu>
2023-08-07 20:39:43 -0700

    made 'confirm logout' bolded and made logout modal exitable by clicking outside modal

--- src/components/dashboard/LogoutConfirmModal.jsx
+++ src/components/dashboard/LogoutConfirmModal.jsx
@@ -1,17 +1,63 @@
-import React from "react";
+// import React from "react";
+// import styles from "../../styles/Dashboard.module.css";
+
+// const LogoutConfirmModal = ({ isOpen, handleClose, handleLogout }) => {
+
+//   if (!isOpen) {
+//     return null;
+//   }
+
+//   return (
+//     <div className={styles["logoutModalOverlay"]}>
+//       <div className={styles["logoutModal"]}>
+//         <h1>Confirm Logout</h1>
+//         <p>Are you sure you want to log out?</p>
+//         <div classname={styles["logout-button-container"]}>
+//           <button className={styles["confirm"]} onClick={handleLogout}>
+//             {" "}
+//             <span className={styles["highlight"]}>Confirm</span>
+//           </button>
+//           <button className={styles["cancel"]} onClick={handleClose}>
+//             Cancel
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LogoutConfirmModal;
+
+import React, { useEffect, useRef } from "react";
 import styles from "../../styles/Dashboard.module.css";
 
 const LogoutConfirmModal = ({ isOpen, handleClose, handleLogout }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (isOpen && modalRef.current && !modalRef.current.contains(event.target)) {
+        handleClose()
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
 
   return (
     <div className={styles["logoutModalOverlay"]}>
-      <div className={styles["logoutModal"]}>
+      <div className={styles["logoutModal"]} ref={modalRef}>
         <h1>Confirm Logout</h1>
         <p>Are you sure you want to log out?</p>
-        <div classname={styles["logout-button-container"]}>
+        <div className={styles["logout-button-container"]}>
           <button className={styles["confirm"]} onClick={handleLogout}>
             {" "}
             <span className={styles["highlight"]}>Confirm</span>
