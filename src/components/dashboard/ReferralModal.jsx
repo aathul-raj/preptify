0866ffe596aee854be19ed631842ccf883be493c
SamYu0310 <shy1@rice.edu>
2023-08-22 22:08:30 -0500

    referal

---
+++ src/components/dashboard/ReferralModal.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+import copyImg from '../../img/copy.png'
+import exitImg from '../../img/exit.png'
+import styles from "../../styles/Dashboard.module.css";
+
+const ReferralModal = ({ isOpen, handleClose, copyLink }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (isOpen && modalRef.current && !modalRef.current.contains(event.target)) {
+        handleClose()
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className={styles["referralModalOverlay"]}>
+      <div className={styles["referralModal"]} ref={modalRef}>
+        <img src={exitImg} className={styles["exitReferralModal"]} onClick={handleClose}/>
+        <h1><span className={styles["green"]}>Invite</span> Your Friends!</h1>
+        <p>Share the love! Introduce your friends to Zara.</p>
+        <button onClick={copyLink}>
+          <img src={copyImg} className={styles["buttonImg"]}/>
+          <p className={styles["buttonWriting"]}>copy link</p>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ReferralModal;
--- src/constants/SidebarImages.jsx
+++ src/constants/SidebarImages.jsx
@@ -1,11 +1,13 @@
 import Menu from '../img/icons/dashboard.png'
 import Settings from '../img/icons/settings.png'
 import Logout from '../img/icons/logout.png'
+import Referral from '../img/icons/referral.png'
 
 const SidebarImages = {
   menu: Menu,
   settings: Settings,
   logout: Logout,
+  referral: Referral
 };
 
 export default SidebarImages;
